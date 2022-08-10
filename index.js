1e9b2617d7121d9b905620946da42bdbf0a58bcb
Austin Barrington <arbarrington12@gmail.com>
2022-08-09 20:12:10 -0600

    commit tree d33446e8a447d8cea9970f363a714a1fa88af467

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+
+function writeCards(name,event) {
+    let array1 = [];
+    for (const element of name) {
+    array1.push(`Thank you, ${element}, for the wonderful ${event} gift!`);
+    }  return array1;
+}
+
+function countDown(num1) {
+    for (let i = num1; i > -1; i--) {
+        console.log(i);
+    }
+}
